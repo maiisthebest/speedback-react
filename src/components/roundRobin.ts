3670db0e2807190e7386f303cdedc3c8ea31a6fa
Phuong Mai Nguyen <phuong.nguyen@aotech.net.au>
2025-06-23 22:17:53 +1000

    chore: Refactor

--- src/components/roundRobin.ts
+++ src/components/roundRobin.ts
@@ -1,24 +1,21 @@
 export const generateRounds = (participants: string[]): string[][] => {
-	const numRounds =
-		participants.length % 2 === 0
-			? participants.length - 1
-			: participants.length;
+	const isEven = participants.length % 2 === 0;
+	const numRounds = isEven ? participants.length - 1 : participants.length;
 
 	let rounds: string[][] = [];
-	let arr = [...participants];
+	let players = [...participants];
 
-	for (let i = 0; i < numRounds; i++) {
+	for (let round = 0; round < numRounds; round++) {
 		const roundMatches: string[] = [];
 
-		for (let j = 0; j < participants.length / 2; j++) {
-			const home = arr[j];
-			const away = arr[participants.length - 1 - j];
+		for (let j = 0; j < players.length / 2; j++) {
+			const home = players[j];
+			const away = players[players.length - 1 - j];
 
 			roundMatches.push(`${home} + ${away}`);
 		}
-		console.log(roundMatches);
 		rounds.push(roundMatches);
-		arr = [arr[0], ...arr.slice(2), arr[1]];
+		players.splice(1, 0, players.pop()!);
 	}
 
 	return rounds;
